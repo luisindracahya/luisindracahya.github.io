73d78a81bb89563b41800f106db16e5ef3cafcc9
FlerkenFury <60496179+FlerkenFury@users.noreply.github.com>
2020-10-31 17:42:25 +0700

    test

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: false
+});
+
+// /*SCROLL HOME*/
+// sr.reveal('.home__title',{}); 
+// sr.reveal('.button',{delay: 200}); 
+// sr.reveal('.home__img',{delay: 400}); 
+// sr.reveal('.home__social-icon',{ interval: 200}); 
+
+// /*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+
+// // /*SCROLL exp*/
+sr.reveal('.intro',{});
+sr.reveal('.group',{delay:600}); 
+sr.reveal('.exp__data',{delay: 1200}); 
+
+// // /*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+// // /*SCROLL CONTACT*/
+sr.reveal('.card',{interval: 200}); 
+
+const words = ["Hello", "Halo", "Bonjour", "Hola", "你好", "Ciao", "こんにちは", "Xin chào", "Guten Tag", "안녕하세요"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 400);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
+
+typingEffect();
